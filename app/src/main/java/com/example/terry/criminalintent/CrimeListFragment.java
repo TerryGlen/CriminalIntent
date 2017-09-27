e92c03aeccf1285d555abd5ffebfbc5dd3fb34b2
Terry Glinsey <saispas99@gmail.com>
2017-09-27 03:19:40 -0500

    Added Abstract Activity

---
+++ app/src/main/java/com/example/terry/criminalintent/CrimeListFragment.java
@@ -0,0 +1,11 @@
+package com.example.terry.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Terry on 9/27/2017.
+ */
+
+public class CrimeListFragment extends Fragment {
+    //Nothing Yet
+}
